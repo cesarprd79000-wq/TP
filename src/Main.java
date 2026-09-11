c67e498239c0e87d6e9f86bdf8ba6916b1e38ee9
PIERRARD César <cesar.pierrard.auditeur@lecnam.net>
2026-09-11 11:20:38 +0200

    Exercice 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        // EXERCICE 1
         // 5. Création de 2 livres
         Livre livre1 = new Livre("Le Comte de Monte-Cristo", "Alexandre Dumas", 1200);
         Livre livre2 = new Livre("1984", "George Orwell", 328);
@@ -12,5 +14,31 @@ public class Main {
 
         livre1.rendre();
         livre1.emprunter();
+
+        //EXERCICE 2
+        CompteBancaire compte = new CompteBancaire("César", 100.0);
+        System.out.println("Solde initial : " + compte.getSolde() + " €");
+
+        compte.deposer(50.0);
+        System.out.println("Après dépôt de 50€ : " + compte.getSolde() + " €");
+
+        compte.retirer(30.0);
+        System.out.println("Après retrait de 30€ : " + compte.getSolde() + " €");
+
+        // Test dépôt invalide
+        try {
+            compte.deposer(-10.0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur dépôt : " + e.getMessage());
+        }
+
+        // Test retrait supérieur au solde
+        try {
+            compte.retirer(200.0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur retrait : " + e.getMessage());
+        }
+
+        System.out.println("Solde final : " + compte.getSolde() + " €");
     }
 }
